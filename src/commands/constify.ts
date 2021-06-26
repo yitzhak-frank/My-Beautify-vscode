3c515bf5b3a573e9004e6ee1ed6c39638435c235
yitzhak-frank <yizhakfrank@gmail.com>
2021-06-26 23:35:41 +0300

    constify - scopes & comments

--- src/commands/constify.ts
+++ src/commands/constify.ts
@@ -1,9 +1,19 @@
 import { isComment, isMultilineCommentEnds, isMultilineCommentStarts } from "../helpers/match";
 
-let isConstant: boolean, scopes: number, comment: boolean;
+const 
+    IS_LINE_HAS_FUNCTION_REGEX = /((^| +|\()function\b)|=>/, 
+    VARIABLE_NAME_REGEX = /(?<=\b(let|var)\s)(\w+)/,
+    scopes: { [inner: string]: number, outer: number } = { inner: 0, outer: 0 };
+let 
+    isConstant: boolean, 
+    comment: boolean, 
+    isInOuterFun: boolean;
 
 const constify = (lines: string[]): string[] => {
     lines.forEach((line, x) => {
+
+        if(!isInOuterFun) { isInOuterFun = !!line.match(IS_LINE_HAS_FUNCTION_REGEX); }
+
         const declaration = line.match(/^(| +)(let\b|var\b)/);
 
         if(isMultilineCommentStarts(line)) { comment = true; }
@@ -18,26 +28,58 @@ const constify = (lines: string[]): string[] => {
         if(isThereAreMultipleVariables(line, lines[x+1] || '')) {
             variables = getAllVariables(lines.slice(x, lines.length));
             if(!isConstant) { return; }
-        } else { variables = [(line.match(/(?<=\b(let|var)\s)(\w+)/) || [''])[0]]; }
+        } else { variables = [(line.match(VARIABLE_NAME_REGEX) || [''])[0]]; }
 
         if(!variables[0]) { return; }
         if(!(variables.length-1) && isNoValueAssign(line, variables[0])) { return; }
 
-        const LINES_AFTER = lines.slice(x + variables.length, lines.length);
+        const LINES_AFTER = lines.slice(x + variables.length, lines.length).filter(line => !line.match(/^( +|)\/\//));
         if(!(variables.length-1)) { LINES_AFTER.unshift(line.substring(line.indexOf('='), line.length)); }
-
-        scopes = 0;
         
-        for(let line of LINES_AFTER) {
-            if(isScopeEnded(line, variables)) { break; }
-            if(isVariableValueChange(line, variables)) { break; }
-        }
+        checkLinesAfter(LINES_AFTER, variables);
+
         if(isConstant) { 
             lines[x] = line.replace(declaration[0].replace(/ +/, ''), 'const'); 
         }
     });
 
     return lines;
+};
+
+const checkLinesAfter = (lines: string[], variables: string[]) => {
+    scopes.inner = 0;
+    // Is new function starts
+    let isInInnerFun: boolean = false;
+    // New function variables with the same name as the global variables
+    const innerFunVars: string[] = [];
+
+    for(let line of lines) {
+        if(isMultilineCommentStarts(line)) { comment = true; }
+        if(isMultilineCommentEnds(line)) { comment = false; }
+    
+        if(comment || isComment(line)) { continue; }
+
+        if(line.match(IS_LINE_HAS_FUNCTION_REGEX)) { isInInnerFun = true; }
+        if(isInOuterFun) { 
+            if(isScopeEnded(line, 'outer', variables)) { 
+                isInOuterFun = false;
+                scopes.outer = 0;
+                continue; 
+            } 
+        }
+        if(isInInnerFun) { 
+            const variable = (line.match(VARIABLE_NAME_REGEX)||[''])[0];
+            if(variables.includes(variable)) { 
+                variables = variables.filter(variable => innerFunVars.includes(variable));
+                innerFunVars.push(variable); 
+            }
+            if(isScopeEnded(line, 'inner', variables)) { 
+                isInInnerFun = false; 
+                variables.push(...innerFunVars);
+            } 
+        }
+        if(isVariableValueChange(line, variables)) { break; }
+    }
 };
 
 const isNoValueAssign = (line: string, variable: string): boolean => {
@@ -49,16 +91,16 @@ const isNoValueAssign = (line: string, variable: string): boolean => {
     }
 };
 
-const isScopeEnded = (line: string, variables: string[]): boolean => {
+const isScopeEnded = (line: string, scope: string, variables: string[]): boolean => {
     const openBrackets = line.match(/{/g) || [], closeBrackets = line.match(/}/g) || [];
-    scopes += (closeBrackets.length - openBrackets.length);
+    scopes[scope] += (closeBrackets.length - openBrackets.length);
 
-    if (scopes > 0) { 
+    if (scopes[scope] >= 0) { 
         const indexes: number[] = [];
         line.split('').forEach((char: string, i: number) => {
             if(char === '}') { indexes.push(i); }; 
         });
-        line = line.slice(0, indexes[closeBrackets.length - scopes]);
+        line = line.slice(0, indexes[closeBrackets.length - scopes[scope]]);
         isVariableValueChange(line, variables);
         return true; 
     } else { return false; }
@@ -70,7 +112,7 @@ const isVariableValueChange = (line: string, variables: string[]): boolean => {
 
         if(line.match(VARIABLE_EXIST)) {
 
-            const VALUE_CHANGE = new RegExp(`.*([-+]{2}(| +)${variable})|(^|[^.])(${variable}(| +)((?=[^==])([-*\/+%=]{2})|(=[^==])))`);
+            const VALUE_CHANGE = new RegExp(`.*([-+]{2}(| +)${variable})|( +|^|\\(|\\)|\\{|>|;|,|:|\\?|\\||&)(${variable}(| +)((?=[^==])([-*\/+%=]{2})|(=[^==])))`);
             const ENDS_WITH_EQUAL = new RegExp(`(( |^)${variable})(=|= +| +=| += +)$`);
 
             if(line.replace(/ +/g, '').match(VALUE_CHANGE) || line.match(ENDS_WITH_EQUAL)) { 
